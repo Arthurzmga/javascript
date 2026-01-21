bcb13052df5f29e68a82196b0c7226476ee64ff3
Arthur <arthurzmga@gmail.com>
2026-01-21 10:02:22 -0300

    Avanço nos estudos

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida de 60 km/h`)
+    console.log('MULTADO! Você deve pagar uma multa de R$ 350,00')
+}
+console.log('Dirija sempre usando cinto de segurança!')
