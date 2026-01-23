a56990cb59fd1de13197ac21e94589375f7303e8
Arthur <arthurzmga@gmail.com>
2026-01-23 10:21:04 -0300

    Avanço

---
+++ aula13/ambiente.js
@@ -0,0 +1,25 @@
+var c = 1
+do{
+    console.log("Ambiente de desenvolvimento configurado com sucesso!" + c);
+    c++;
+} while (c <= 10) 
+
+
+
+
+
+
+
+
+
+
+
+
+    
+/*
+var c = 1
+while (c <= 500) {
+    console.log("Ambiente de desenvolvimento configurado com sucesso!" + c);
+    c++;
+}
+*/
---
+++ aula14/ambiente.js
@@ -0,0 +1,7 @@
+for (var c = 1; c <= 10; c++) {
+    if (c % 2 == 0) {
+        console.log("B - " + c);
+    }else {
+        console.log("A - " + c);
+    }
+}
